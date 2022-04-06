372064132b521b30f2d6de85409fa3adad15a1e6
Ashley Hodge <ashhodge@comcast.net>
2022-04-06 16:14:39 -0400

    small change

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,7 +3,7 @@ const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 // get all posts from a single user
-router.get('/', (req, res) => {
+router.get('/', withAuth,(req, res) => {
   Post.findAll({
     where: {
       user_id: req.session.user_id
